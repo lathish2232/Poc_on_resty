63464c5064c72f819c0be1cc4bfc73e7faf85686
Lathish <44872859+lathish2232@users.noreply.github.com>
2021-09-22 12:49:17 +0530

    Update settings.py

--- settings.py
+++ settings.py
@@ -7,6 +7,6 @@ logger = logging.getLogger("  Mobi service message   ")
 
 
 database = {
-    "SQLALCHEMY_DATABASE_URI": "postgresql://uniuser_1:12345@35.223.205.190:5432/mobikwik",
+    "SQLALCHEMY_DATABASE_URI": "postgresql://user:password@host:port/database",
     "SQLALCHEMY_TRACK_MODIFICATIONS": False
 }
